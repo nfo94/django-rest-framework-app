5666c6fb7778103d9c78f92678720a0683fba78f
Natália Oliveira <nataliaferreiraoliveira1994@gmail.com>
2023-06-15 06:52:16 +0100

    chore: configure openapi docs and schema

--- app/app/settings.py
+++ app/app/settings.py
@@ -133,4 +133,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "core.User"
 
-REST_FRAMEWORK = {"DEFAULT_SCHEMA_CLASS": "drf_spectactular.openapi.AutoSchema"}
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
