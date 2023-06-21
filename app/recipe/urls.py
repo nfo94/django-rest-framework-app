ac572e2fe079b572dce884a5f741274c46fcc9d7
Natália Oliveira <nataliaferreiraoliveira1994@gmail.com>
2023-06-21 08:31:21 +0100

    chore: add recipe api logic

---
+++ app/recipe/urls.py
@@ -0,0 +1,12 @@
+from django.urls import include, path
+from recipe import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register("recipes", views.RecipeViewSet)
+
+app_name = "recipe"
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
