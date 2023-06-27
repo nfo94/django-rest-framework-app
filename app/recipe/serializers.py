d228c237bb08f5123f96236e629ad1d798234aa3
Natália Oliveira <nataliaferreiraoliveira1994@gmail.com>
2023-06-27 10:36:17 +0100

    chore: add recipe detail view and serializer

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -15,3 +15,8 @@ class RecipeSerializer(serializers.ModelSerializer):
         read_only_fields = [
             "id",
         ]
+
+
+class RecipeDetailSerializer(RecipeSerializer):
+    class Meta(RecipeSerializer.Meta):
+        fields = RecipeSerializer.Meta.fields + ["description"]
